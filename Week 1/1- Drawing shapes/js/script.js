8abbd07a78a79fecee38dd11225adccf05bfb837
megsummmers <70597858+megsummmers@users.noreply.github.com>
2020-09-16 17:23:14 -0400

    Added Week 1 folder + drawing folder
    Created a new folder within repository for week 1 and included the assignment of video 2.2 drawing shapes

---
+++ Week 1/1- Drawing shapes/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(255,0,0);//RGB for colour red
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
