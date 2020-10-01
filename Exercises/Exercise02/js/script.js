03589ee14c3731866cf266361ba9be14c1ff748b
megsummmers <70597858+megsummmers@users.noreply.github.com>
2020-10-01 14:08:59 -0400

    Continued working on exercise 2
    Wrote most of the extra code

--- Exercises/Exercise02/js/script.js
+++ Exercises/Exercise02/js/script.js
@@ -7,21 +7,23 @@ Rest of the code by Meg Summers
 
 //VARIABLES
 let bg = {
-  r: 0
+  r: 0,
+  g: 0,
+  b: 0,
 }
 
 let covid19 = {
   x: 0,
   y: 500, //check if you can put a random in there
   size: 200,
-  speed: 8,
-  r: 255,
-  growthRate: 5
-  //let y = random(0, 800);
+  speed: 15,
+  r: 97,
+  g: 69,
+  b: 63,
+  growthRate: 10
 };
 
 let user = {
-  size: 150,
   b: 200,
   r: 0
 };
@@ -43,15 +45,14 @@ function setup() {
 //contains if/else statements
 function draw() {
   //background
-  background(bg.r,0,0);
+  background(bg.r, bg.g, bg.b);
   //static
 
-  //USER circle
-  fill(user.r, 0, user.b);
-  ellipse(mouseX, mouseY, user.size);
+  //USER spaceship
+  image(img, mouseX, mouseY);
 
   //COVID19 circle
-  fill(covid19.r, 0, 0);
+  fill(covid19.r, covid19.g, covid19.b);
   ellipse(covid19.x, covid19.y, covid19.size);
 
   //movement
@@ -61,10 +62,15 @@ function draw() {
     //covid19
     covid19.size = covid19.size + covid19.growthRate;
     covid19.size = constrain(covid19.size, 200, width);
-    //chnages user circle colour
-    user.r = user.r + 1;
+    covid19.r = covid19.r +3
+    covid19.g = covid19.g +3
+    covid19.b = covid19.b +3
+
     //background colour change
-    bg.r = bg.r + 1;
+    bg.r = bg.r + 3;
+    bg.g = bg.g + 3;
+    bg.b = bg.b + 3;
+
     //Game Over text
     fill(0);
     textAlign(CENTER);
@@ -81,8 +87,31 @@ function draw() {
 
   //Circles overlap if
   let d = dist(mouseX, mouseY, covid19.x, covid19.y);
-  if(d <= 200){
+  if(d <= 150){
     user.b = user.b - 200;
     gameOver = true;
   }
+
+  //if statement for colour change
+  if(!gameOver){
+    if(d > 500){
+      covid19.r = covid19.r - 10;
+      covid19.r = constrain(covid19.r, 10, 97);
+      covid19.g = covid19.g - 10;
+      covid19.g = constrain(covid19.g, 10, 69);
+      covid19.b = covid19.b - 10;
+      covid19.b = constrain(covid19.b, 10, 63);
+    } else if (d < 500){
+      covid19.r = covid19.r + 10;
+      covid19.r = constrain(covid19.r, 10, 97);
+      covid19.g = covid19.g + 10;
+      covid19.g = constrain(covid19.g, 10, 69);
+      covid19.b = covid19.b + 10;
+      covid19.b = constrain(covid19.b, 10, 63);
+    }
+  }
 }//draw }
+
+function preload(){
+  img = loadImage('assets/images/8-bit-spaceship.PNG');
+}
