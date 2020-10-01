4125b2def6ed8f3233fabd40ceffbeb71e578f7e
megsummmers <70597858+megsummmers@users.noreply.github.com>
2020-10-01 19:24:51 -0400

    Finished Exercise 02
    Added background meteors for more interesting gameplay
    Fixed pointerlock issue
    etc

--- Exercises/Exercise02/js/script.js
+++ Exercises/Exercise02/js/script.js
@@ -11,29 +11,56 @@ let bg = {
   g: 0,
   b: 0,
 };
-
-let imag = {
+let spaceship = {
+  alpha: 255,
   x: 900,
   y: 900
 };
-
-let covid19 = {
+let meteor = {
+  alpha: 0,
   x: 0,
   y: 500,
   size: 300,
-  speed: 18,
+  speed: 14,
   r: 97,
   g: 69,
   b: 63,
   growthRate: 10
 };
-
-let user = {
-  b: 200,
-  r: 0
+let craters = {
+  alpha: 0,
+  r: 48,
+  g: 39,
+  b: 37,
+  size1: 100,
+  size2: 40,
+  size3: 75,
+  size4: 80,
+  x1: -50,
+  y1: 550,
+  x2: 60,
+  y2: 425,
+  x3: 75,
+  y3: 520,
+  x4: -45,
+  y4: 425
 };
-
+let bgMeteor = {
+  alpha: 125,
+  x: 0,
+  y: 200,
+  x2: 0,
+  y2: 800,
+  size: 100,
+  size2: 175,
+  speed: 10,
+  speed2: 5,
+  r: 67,
+  g: 39,
+  b: 33
+};
 let gameOver = false;
+let starAmount = 250;
 
 // setup()
 //
@@ -54,23 +81,70 @@ function draw() {
   //-------- BACKGROUND SETUP --------
   background(bg.r, bg.g, bg.b);
 
+  //-------- BG METOR SETUP --------
+  fill(bgMeteor.r, bgMeteor.g, bgMeteor.b, bgMeteor.alpha);
+  ellipse(bgMeteor.x, bgMeteor.y, bgMeteor.size);
+  ellipse(bgMeteor.x2, bgMeteor.y2, bgMeteor.size2);
+
+  //-------- Stars/Static --------
+  for(let i = 0; i <= starAmount; i++){
+    push();
+    stroke(255);
+    strokeWeight(2)
+    let x = random(0, width);
+    let y = random(0, height);
+    point(x, y);
+    pop();
+  }
+
   //-------- SPACESHIP SETUP --------
-  image(img, imag.x, imag.y);
+  image(img, spaceship.x, spaceship.y, spaceship.alpha);
 
   //-------- METOR SETUP --------
-  fill(covid19.r, covid19.g, covid19.b);
-  ellipse(covid19.x, covid19.y, covid19.size);
+  push();
+  strokeWeight(5);
+  stroke(craters.r, craters.g, craters.b, craters.alpha);
+  fill(meteor.r, meteor.g, meteor.b, meteor.alpha);
+  ellipse(meteor.x, meteor.y, meteor.size);
+  pop();
+  //-------- CRATER SETUP --------
+  fill(craters.r, craters.g, craters.b, craters.alpha);
+  ellipse(craters.x1, craters.y1, craters.size1);
+  ellipse(craters.x2, craters.y2, craters.size2);
+  ellipse(craters.x3, craters.y3, craters.size3);
+  ellipse(craters.x4, craters.y4, craters.size4);
 
   //-------- METOR MOVEMENT --------
   if(!gameOver){
-    covid19.x = covid19.x + covid19.speed;
+    meteor.x = meteor.x + meteor.speed;
+    craters.x1 = craters.x1 + meteor.speed;
+    craters.x2 = craters.x2 + meteor.speed;
+    craters.x3 = craters.x3 + meteor.speed;
+    craters.x4 = craters.x4 + meteor.speed;
+    //-------- BG METOR MOVEMENT --------
+    bgMeteor.x = bgMeteor.x + bgMeteor.speed;
+    bgMeteor.x2 = bgMeteor.x2 + bgMeteor.speed2;
+
   } else if(gameOver){ //GAMEOVER SECTION
     //-------- METOR GAMEOVER --------
-    covid19.size = covid19.size + covid19.growthRate;
-    covid19.size = constrain(covid19.size, 200, width);
-    covid19.r = covid19.r +3
-    covid19.g = covid19.g +3
-    covid19.b = covid19.b +3
+    meteor.size = meteor.size + meteor.growthRate;
+    meteor.size = constrain(meteor.size, 200, width);
+    meteor.r = meteor.r +3
+    meteor.g = meteor.g +3
+    meteor.b = meteor.b +3
+
+    //-------- BG METOR GAMEOVER --------
+    bgMeteor.alpha = bgMeteor.alpha - 150;
+
+    //-------- CRATER GAMEOVER --------
+    craters.size1 = craters.size1 + meteor.growthRate;
+    craters.size2 = craters.size2 + meteor.growthRate;
+    craters.size3 = craters.size3 + meteor.growthRate;
+    craters.size4 = craters.size4 + meteor.growthRate;
+    craters.alpha = craters.alpha - 15;
+
+    //-------- SPACESHIP GAMEOVER --------
+    spaceship.alpha = spaceship.alpha - 255;
 
     //-------- BACKGROUND GAMEOVER --------
     bg.r = bg.r + 3;
@@ -86,37 +160,55 @@ function draw() {
   }
 
   //-------- METOR RESET/RANDOM --------
-  if(covid19.x >= width +150){
-    covid19.x = covid19.x - width -200;
-    covid19.y = random(0, height);
+  if(meteor.x >= width +150){
+    let meteorY = random(0, height);
+    meteor.x = meteor.x - width -200;
+    craters.x1 = craters.x1 - width -200;
+    craters.x2 = craters.x2 - width -200;
+    craters.x3 = craters.x3 - width -200;
+    craters.x4 = craters.x4 - width -200;
+    meteor.y = meteorY;
+    craters.y1 = meteorY +50;
+    craters.y2 = meteorY -75;
+    craters.y3 = meteorY +20;
+    craters.y4 = meteorY -75;
+  }
+
+  //-------- BG METOR RESET/RANDOM --------
+  if(bgMeteor.x >= width + 100){
+    let bgMeteorY = random(0, height);
+    bgMeteor.y = bgMeteorY;
+    bgMeteor.x = bgMeteor.x - width -100;
+  }
+  if(bgMeteor.x2 >= width +100){
+    let bgMeteorY2 = random(0, height);
+    bgMeteor.y2 = bgMeteorY2;
+    bgMeteor.x2 = bgMeteor.x2 - width -100;
   }
 
   //-------- OVERLAP CHECK --------
-  let d = dist(mouseX, mouseY, covid19.x, covid19.y);
+  //change to spaceship coordinate
+  let d = dist(spaceship.x, spaceship.y, meteor.x, meteor.y);
   if(d <= 175){
-    user.b = user.b - 200;
     gameOver = true;
   }
 
   //-------- PROXIMITY METEOR COLOUR CHANGE --------
   if(!gameOver){
-    if(d > 500){
-      covid19.r = covid19.r - 10;
-      covid19.r = constrain(covid19.r, 10, 97);
-      covid19.g = covid19.g - 10;
-      covid19.g = constrain(covid19.g, 10, 69);
-      covid19.b = covid19.b - 10;
-      covid19.b = constrain(covid19.b, 10, 63);
-    } else if (d < 500){
-      covid19.r = covid19.r + 10;
-      covid19.r = constrain(covid19.r, 10, 97);
-      covid19.g = covid19.g + 10;
-      covid19.g = constrain(covid19.g, 10, 69);
-      covid19.b = covid19.b + 10;
-      covid19.b = constrain(covid19.b, 10, 63);
+    if(d > 700){
+      meteor.alpha = meteor.alpha - 15;
+      meteor.alpha = constrain (meteor.alpha, 0, 255);
+      craters.alpha = craters.alpha - 15;
+      craters.alpha = constrain (craters.alpha, 0, 255);
+    } else if (d < 700){
+      meteor.alpha = meteor.alpha + 15;
+      meteor.alpha = constrain (meteor.alpha, 0, 255);
+      craters.alpha = craters.alpha + 15;
+      craters.alpha = constrain (craters.alpha, 0, 255);
     }
   }
 }
+
 //-------- IMAGE FUNCTION --------
 function preload(){
   img = loadImage('assets/images/8-bit-spaceship.PNG');
@@ -124,14 +216,6 @@ function preload(){
 //-------- MOUSE DRAG FUNCTION --------
 function mouseDragged(){
   //allows spaceship to move while mouse is dragged
-  imag.x = mouseX;
-  imag.y = mouseY;
-  //locks the cursor to the last coodinates
-  if(!locked){
-    locked = true;
-    requestPointerLock();
-  } else {
-    exitPointerLock();
-    locked = false;
-  }
+  spaceship.x += movedX;
+  spaceship.y += movedY;
 }
