294270ecc66219696c0079257b5d5d24f73a1086
megsummmers <70597858+megsummmers@users.noreply.github.com>
2020-10-08 14:21:39 -0400

    E3: Re-organized to add state
    created title screen then added states to better transition between title screen, end screen and gameplay.
    Moved code around to better accomadate the states

--- Exercises/Exercise03/js/script.js
+++ Exercises/Exercise03/js/script.js
@@ -7,10 +7,6 @@ Make your changes to the simulation with the following requirements:
 Add at least one extra function
 Not including functions any built-in p5 functions like keyPressed()
 Maybe you could add a function that checks if one circle has grown too large (if they grow) or shrunk too small (if they shrink), or faded too much (if their alpha fades)
-Add at least one extra ending
-Maybe it could be an “easter egg” and hard to discover? (Moving the mouse to a really specific location?)
-Maybe it offers a different dimension of thinking about love and loss?
-Maybe it’s connected to the new function in the previous step?
 **************************************************/
 
 //VARIABLES
@@ -40,28 +36,88 @@ let bg = {
   r: 0,
   g: 0,
   b: 0
-}
-
-let endScreen = 0;
+};
 
 let brokenHearts = 0;
 
+//add state
+let state = 'title';
+
 // setup()
 //
-//
+// Creates canvas, sets no stroke and preloads the image
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-
   preLoad();
 }
+
 // draw()
 //
-//
+// sets and moves the two circles
+// controls the pixel heart lives
 function draw() {
+  //----- BACKGROUND SETUP -----
   background(bg.r, bg.g, bg.b);
-  //----- RANDOM LOVECIRCLE SET-----
 
+  //----- STATE SETUP -----
+  if(state === 'title'){
+    titleScreen();
+  }
+  else if (state === 'gameplay'){
+    gameplay();
+  }
+  else if (state === 'ending'){
+    //----- NUM OF LIVES CHECK -----
+      if(brokenHearts <= 2){
+        ending(brokenHearts, 255, 115, 171);
+      } else if (brokenHearts === 3 || brokenHearts === 4){
+        ending(brokenHearts, 155, 100, 120);
+      } else if (brokenHearts === 5){
+        ending(brokenHearts, 70, 50, 60);
+      }
+  }
+}
+
+//----- IMAGE PRELOAD -----
+function preLoad(){
+  img = loadImage ('assets/images/pixel-heart.PNG');
+}
+
+//----- TITLE SCREEN -----
+function titleScreen(){
+  fill(255, 133, 177);
+  rectMode(CENTER);
+  rect(width/2, height/2, windowWidth, windowHeight);
+  textAlign(CENTER);
+  fill(255);
+  textSize(80);
+  text('Welcome to looking for Love', width/2, height/3);
+  textSize(50);
+  text("For info on how to play, press 'H'", width/2, height/2);
+  text("To begin the game, press 'P'", width/2, height/2 + 50);
+  if(key === 'h'){
+    fill(255, 133, 177);
+    rectMode(CENTER);
+    rect(width/2, height/2, windowWidth, windowHeight);
+    textAlign(CENTER);
+    fill(255);
+    textSize(80);
+    text('HOW TO PLAY:', width/2, height/2 - 100);
+    textSize(25);
+    text('Your goal is to find and touch the other circle before it shrinks away', width/2, height/2);
+    text('To control the grey circle; use the arrow keys (up, down, left and right)', width/2, height/2 + 40);
+    text('Each time the circle fully shrinks it will change positions and one of your hearts will disappear', width/2, height/2 + 80);
+    text("If you can't find the other heart before it shrinks 5 times, you lose.", width/2, height/2 + 120);
+    textSize(50);
+    text("To begin playing, press 'P'", width/2, height/2 + 160);
+  } else if (key === 'p' && state === 'title'){
+    state = 'gameplay';
+  }
+}
+
+//----- GAMEPLAY SECTION -----
+function gameplay(){
   //----- USER SETUP -----
   fill(user.r, user.g, user.b, user.alpha);
   ellipse(user.x, user.y, user.size);
@@ -71,30 +127,28 @@ function draw() {
   ellipse(loveCircle.x, loveCircle.y, loveCircle.size);
 
   //----- LIVES SETUP -----
-  if (endScreen === 0){
-    endScreen = livesCounter(brokenHearts);
-  }
+  livesCounter(brokenHearts);
 
   //----- USER MOVEMENT CONTROL -----
-  if (keyIsDown(UP_ARROW) && endScreen === 0) {
+  if (keyIsDown(UP_ARROW)) {
     user.y = user.y - user.speed;
     user.y = constrain(user.y, 0, height);
-  } else if (keyIsDown(DOWN_ARROW) && endScreen === 0) {
+  } else if (keyIsDown(DOWN_ARROW)) {
     user.y = user.y + user.speed;
     user.y = constrain(user.y, 0, height);
-  } else if (keyIsDown(LEFT_ARROW) && endScreen === 0) {
+  } else if (keyIsDown(LEFT_ARROW)) {
     user.x = user.x - user.speed;
     user.x = constrain(user.x, 0, width);
-  } else if (keyIsDown(RIGHT_ARROW) && endScreen === 0) {
+  } else if (keyIsDown(RIGHT_ARROW)) {
     user.x = user.x + user.speed;
     user.x = constrain(user.x, 0, width);
   }
 
   //----- LOVECIRCLE SHRINK -----
-  if(loveCircle.size >= 1 && endScreen === 0){
+  if(loveCircle.size >= 1){
     loveCircle.size = loveCircle.size - loveCircle.shrink;
     loveCircle.size = constrain(loveCircle.size, 0, 150);
-  } else if(loveCircle.size <= 0 && endScreen === 0){
+  } else if(loveCircle.size <= 0){
     loveCircle.size += 150;
     brokenHearts += 1;
     loveCircle.x = random(0, width);
@@ -105,33 +159,19 @@ function draw() {
   let d = dist(user.x, user.y, loveCircle.x, loveCircle.y);
   if (d <= 150){
     //----- GAME OVER -----
-    endScreen = 1;
+    state = 'ending';
     //----- PROXIMITY ALPHA -----
-  } else if (d > 300 && endScreen === 0) {
+  } else if (d > 300) {
     loveCircle.alpha -= 10;
     loveCircle.alpha = constrain(loveCircle.alpha, 0, 175);
-  } else if (d < 300 && endScreen === 0) {
+  } else if (d < 300) {
     loveCircle.alpha += 10;
     loveCircle.alpha = constrain(loveCircle.alpha, 0, 175);
   }
-
-  //----- ENDSCREEN CHECK -----
-  if(endScreen === 1){
-    if(brokenHearts <= 2){
-      endGameScreen(brokenHearts, 255, 115, 171);
-    } else if (brokenHearts === 3 || brokenHearts === 4){
-      endGameScreen(brokenHearts, 155, 100, 120);
-    } else if (brokenHearts === 5){
-      endGameScreen(brokenHearts, 70, 50, 60);
-    }
-  }
 }
 
-function preLoad(){
-  img = loadImage ('assets/images/pixel-heart.PNG');
-}
-
-function endGameScreen(num, r, g, b) {
+//----- END GAME SECTION -----
+function ending(num, r, g, b) {
   //lovecircle
   loveCircle.alpha -= 10;
   loveCircle.alpha = constrain(loveCircle.alpha, 0, 255);
@@ -146,20 +186,14 @@ function endGameScreen(num, r, g, b) {
   bg.b += 5;
   bg.b = constrain(bg.b, 0, b);
   //text
+  fill(255);
+  textSize(50);
+  textAlign(CENTER, CENTER);
   if (num <= 2){
-    fill(255);
-    textSize(50);
-    textAlign(CENTER, CENTER);
     text('Congratulations! You are loved <3', width/2, height/2);
   } else if (num === 3 || num === 4){
-    fill(255);
-    textSize(50);
-    textAlign(CENTER, CENTER);
     text('Congratulations! You managed to find love.', width/2, height/2);
   } else if (num === 5){
-    fill(255);
-    textSize(50);
-    textAlign(CENTER, CENTER);
     text('You lost your chance at love...', width/2, height/2);
   }
 }
@@ -167,32 +201,32 @@ function endGameScreen(num, r, g, b) {
 //----- PIXEL HEART LIVES COUNTER -----
 function livesCounter(brokenHearts){
   if(brokenHearts === 0){
+    //5 hearts on screen
     image(img, width/2, 50, 100, 100);
     image(img, width/2 -100, 50, 100, 100);
     image(img, width/2 -200, 50, 100, 100);
     image(img, width/2 +100, 50, 100, 100);
     image(img, width/2 +200, 50, 100, 100);
-    return 0;
   } else if (brokenHearts === 1){
+    //4 hearts on screen
     image(img, width/2, 50, 100, 100);
     image(img, width/2 -100, 50, 100, 100);
     image(img, width/2 -200, 50, 100, 100);
     image(img, width/2 +100, 50, 100, 100);
-    return 0;
   } else if (brokenHearts === 2){
+    //3 hearts on screen
     image(img, width/2, 50, 100, 100);
     image(img, width/2 -100, 50, 100, 100);
     image(img, width/2 -200, 50, 100, 100);
-    return 0;
   } else if (brokenHearts === 3){
+    //2 hearts on screen
     image(img, width/2 -100, 50, 100, 100);
     image(img, width/2 -200, 50, 100, 100);
-    return 0;
   } else if (brokenHearts === 4){
-    image(img, width/2 -100, 50, 100, 100);
-    return 0;
+    //1 hearts on screen
+    image(img, width/2 -200, 50, 100, 100);
   } else if (brokenHearts === 5){
-    return 1;
+    //0 hearts on screen, finishes game
+    state = 'ending';
   }
-
 }
